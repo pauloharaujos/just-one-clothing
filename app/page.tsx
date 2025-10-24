e002e968b9ce62d347d8441b35087fd9951f6bd4
Paulo Henrique Silva <paulo@imdigital.com>
2025-10-24 17:42:54 -0300

    Redesign HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Footer from '@/ui/components/Footer';
 import TopBanner from '@/ui/cms/top-banner';
 import HomeBanner from '@/ui/cms/home-banner';
 import Products from '@/ui/components/home/Products';
+import Features from '@/ui/components/home/Features';
+import Newsletter from '@/ui/components/home/Newsletter';
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen bg-white">
       <TopBanner/>
       <HeaderWrapper />
       <HomeBanner />
       <Products />
+      <Features />
+      <Newsletter />
       <Footer />
     </div>
   );
